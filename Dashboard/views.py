61fcf0a442c88f8ea84d2ab255b6befab1079138
Luis Moran <90297281+Luis-M23@users.noreply.github.com>
2023-06-02 21:56:26 -0600

    Modificacion de las vistas

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,4 +1,49 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from Dashboard.models import categoriaOcupacional,tipoDiscapacidad,sectorEconomico,rubro,rangoEdades,detalleEstadisticoDiscapacitado
 
 def detail(request):
     return render(request, 'dashboards/dashboard.html')
+
+def categoria(request):
+    return render(request, 'dashboards/FormCategoriaOcupacional.html')
+
+
+def vistaCenso(request):
+    return render(request, 'dashboards/censo.html')
+
+def lista_Categoria(request):
+    categorias=categoriaOcupacional.objects.all()
+    return render(request,'dashboards/FormCategoriaOcupacional.html',{'categorias':categorias})
+
+def lista_Discapacidad(request):
+
+    
+    if request.method =='POST':
+        nombre = request.POST['tipoDis']
+        canHombre = request.POST['canHombres']
+        canMujeres = request.POST['canMujeres']
+   
+        detalle = detalleEstadisticoDiscapacitado.objects.create(
+            cantidadHombre=canHombre,
+            cantidadMujeres=canMujeres
+        )
+
+        discapacidad = tipoDiscapacidad.objects.create(
+            nombreDiscapacidad=nombre,
+            detalle=detalle 
+        )
+
+    return render(request,'dashboards/FormTipoDiscapacidad.html')
+
+def lista_SectorEconomico(request):
+    sectorEco=sectorEconomico.objects.all()
+    return render(request,'dashboards/FormSectorEconomico.html',{'sector':sectorEco})
+
+def lista_Rubro(request):
+    rubros=rubro.objects.all()
+    return render(request,'dashboards/FormRubro.html',{'rubro':rubros})
+
+def lista_RangoEdad(request):
+    edades=rangoEdades.objects.all()
+    return render(request,'dashboards/FormRangoEdad.html',{'rango':edades})
--- includata/urls.py
+++ includata/urls.py
@@ -5,5 +5,13 @@ from Dashboard.views import *
 urlpatterns = [
     path('includata/dashboard/', detail),
     path('admin/', admin.site.urls),
+    #path('includata/dashboards/FormCategoria/', categoria),
+   
+    path('includata/dashboards/censo/', vistaCenso),
+    path('includata/dashboards/FormCategoria/', lista_Categoria, name='lista_Categorias'),
+    path('includata/dashboards/FormTipoDiscapacidad/', lista_Discapacidad, name='lista_Discapacidad'),
+    path('includata/dashboards/FormSectorEconomico/', lista_SectorEconomico, name='lista_SectorEconomico'),
+    path('includata/dashboards/FormRubro/', lista_Rubro, name='lista_Rubro'),
+    path('includata/dashboards/FormRangoEdad/', lista_RangoEdad, name='lista_RangoEdad'),
     
 ]
